2326504934609cf69ba626f9e216657d3df8fd4a
vakus <ssud16@o2.pl>
2020-03-04 15:37:16 +0000

    Update core.clj

--- src/matcher_starter/core.clj
+++ src/matcher_starter/core.clj
@@ -8,12 +8,67 @@
   [x]
   (println x "Hello, World!"))
 
-(def grid [[0 0 0 0 0 0 1 0 0 0]
-             [0 0 0 0 0 0 0 0 1 0]
-             [0 0 0 1 0 0 0 0 0 0]
-             [0 0 1 0 0 0 0 0 0 0]
-             [0 0 0 0 0 0 1 0 0 0]
-             [0 0 0 0 0 1 0 0 0 0]
-             [0 0 0 1 1 0 0 0 0 0]
-             [0 0 0 0 0 0 0 0 0 0]
-             [1 0 0 0 0 0 0 0 1 0]])
+(use 'clojure.test)
+
+
+(def grid1 '[
+           [0 0 0 0 0 0 1 0 0 0]
+           [0 0 0 0 0 0 0 0 1 0]
+           [0 0 0 1 0 0 0 0 0 0]
+           [0 0 1 0 0 0 0 0 0 0]
+           [0 0 0 0 0 0 1 0 0 0]
+           [0 0 0 0 0 1 0 0 0 0]
+           [0 0 0 1 1 0 0 0 0 0]
+           [0 0 0 0 0 0 0 0 0 0]
+           [1 0 0 0 0 0 0 0 1 0]])
+
+(def grid-result1 '[
+                       [0 0 0 0 0 1 1 1 1]
+                       [0 0 1 1 0 0 0 1 1]
+                       [0 1 1 1 0 0 0 0 0]
+                       [0 1 1 0 0 1 1 0 0]
+                       [0 0 0 0 1 1 1 0 0]
+                       [0 0 1 1 1 1 0 0 0]
+                       [0 0 1 1 1 0 0 0 0]
+                       [1 0 0 0 0 0 0 1 1]
+                       ])
+
+
+(def grid2 '[
+             [0 0 0]
+             [0 0 0]
+             [0 1 0]
+             [0 0 0]
+             ])
+
+(def grid-result2 '[
+                    [0 0]
+                    [1 1]
+                    [1 1]
+                    ])
+
+(defn translate-grid
+  [grid]
+  (loop [grid grid x '0 y '0 translated '[[]]]
+    (cond
+      (== y (- (count grid) 1))
+        (pop translated)
+      (== x (- (count (nth grid y)) 1))
+        (recur grid 0 (+ y 1) (conj translated '[]))
+      (and
+        (== (nth (nth grid y) x) 0)
+        (== (nth (nth grid y) (+ x 1)) 0)
+        (== (nth (nth grid (+ y 1)) x) 0)
+        (== (nth (nth grid (+ y 1)) (+ x 1)) 0))
+          (recur grid (+ x 1) y
+                 (conj (pop translated) (conj (last translated) 0)))
+      :else
+        (recur grid (+ x 1) y (conj (pop translated) (conj (last translated) 1)))
+      )
+    )
+  )
+
+(deftest translate-grid-test
+         (is (== (compare (translate-grid grid1) grid-result1) 0))
+         (is (== (compare (translate-grid grid2) grid-result2) 0))
+         )
